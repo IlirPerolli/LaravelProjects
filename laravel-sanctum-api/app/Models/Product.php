e2e90f24d0f758daf35498bcd477e97c954c105e
Ilir <ilir_perolli@live.com>
2021-05-29 16:21:55 +0200

    Laravel REST API using sanctum

---
+++ laravel-sanctum-api/app/Models/Product.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+//    protected $table = 'my_products';//nese deshirojme emer tjeter te tabeles
+    protected $fillable = ['name','slug','description','price'];
+}
